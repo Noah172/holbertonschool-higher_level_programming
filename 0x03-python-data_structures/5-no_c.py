0cd71782bf6435320552e8dc823704a9603b331a
Noah172 <jsebastian.garcia1995@gmail.com>
2019-12-17 18:50:57 +0000

    remove the c char

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
     nope = ""
-    n = len(my_string)
-    for i in range(0, n):
+    for i in range(0, len(my_string)):
         if my_string[i] == 'C' or my_string[i] == 'c':
             continue
         nope += my_string[i]
