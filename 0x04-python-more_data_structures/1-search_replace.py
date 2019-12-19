843be97d0c97b9269c58270135ef74cbd5e1f789
Noah172 <jsebastian.garcia1995@gmail.com>
2019-12-19 19:47:56 +0000

    reemplace a value

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    rlist = []
+    rlist = my_list[:]
+    for i in range(0, len(rlist)):
+        if rlist[i] == search:
+           rlist[i] = replace
+    return rlist
