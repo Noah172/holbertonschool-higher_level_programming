ca2b24bdc1ffc9ad56015c9a5f4e66fed2839c08
Noah172 <jsebastian.garcia1995@gmail.com>
2019-12-19 20:22:28 +0000

    square matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if not matrix:
+        return None
+    new_matrix = [row[:] for row in matrix]
+    for i in range(0, len(new_matrix)):
+        for j in range(0, len(new_matrix[i])):
+            new_matrix[i][j] = new_matrix[i][j] ** 2
+    return new_matrix
