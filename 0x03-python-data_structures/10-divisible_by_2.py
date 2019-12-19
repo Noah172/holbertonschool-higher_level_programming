86a0382bbb1d5596ec418aacd7c8e92a2edf33d8
Noah172 <jsebastian.garcia1995@gmail.com>
2019-12-19 03:13:14 +0000

    multiples of 2 in a list.

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    res = []
+    if my_list is None:
+        return
+    for i in my_list:
+        if i % 2 == 0:
+            res.append(True)
+        else:
+            res.append(False)
+    return res
