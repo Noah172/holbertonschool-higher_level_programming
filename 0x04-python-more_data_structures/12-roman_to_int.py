93a8bd10fa0793d3600bfe63d9f05492403f7719
Noah172 <jsebastian.garcia1995@gmail.com>
2019-12-21 22:31:48 +0000

    convertin roman numerals into arabic numerals

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
+    if roman_string and isinstance(roman_string, str):
+        num = [1, 5, 10, 50, 100, 500, 1000]
+        roman = ["I", "V", "X", "L", "C", "D", "M"]
+        result = 0
+        prev = 1001
+        for i in range(len(roman_string)):
+            idx = roman.index(roman_string[i])
+            if prev >= num[idx]:
+                result = result + num[idx]
+                prev = num[idx]
+            else:
+                result = num[idx] - 2 * prev + result
+                prev = num[idx]
+        return result
+    else:
         return 0
-
-    numbers = {
-        "I": 1, "V": 5, "X": 10,
-        "L": 50, "C": 100, "D": 500, "M": 1000
-    }
-    listado = []
-    for valor in roman_string:
-        for key, value in numbers.items():
-            if valor == key:
-                listado.append(value)
-    numlet = len(listado)
-    if numlet <= 0:
-        return None
-    result = 0
-    for i in range(numlet):
-        if i > 0 and listado[i] > listado[i-1]:
-            result += listado[i] - 2 * listado[i-1]
-        else:
-            result += listado[i]
-    return result
