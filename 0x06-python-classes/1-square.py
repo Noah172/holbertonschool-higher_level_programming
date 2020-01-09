7f3eaaa5be1a58125371514040e5543852038af5
Noah172 <jsebastian.garcia1995@gmail.com>
2020-01-09 20:41:26 +0000

    giving a value

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/pyrthon3
 class Square:
     """creating a class square"""
-    def __init__(self, __size="0"):
+    def __init__(self, size="0"):
         """self
         args
         self: to call itself
         size: the size of the square
         """
-        self._Square__size=__size
+        self.__size=size
