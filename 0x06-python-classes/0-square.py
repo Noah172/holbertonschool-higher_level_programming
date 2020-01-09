0331fc40581040c4656ec61d85e3d4b3a7342b47
Noah172 <jsebastian.garcia1995@gmail.com>
2020-01-09 20:39:07 +0000

    only doc

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    """creating a class square"""
+    def __init__(self):
+        """self
+        args
+        self: to call itself
+        size: the size of the square
+        """
