bf1d7eab6cee3a9d469906136546bcb546fe52fa
Noah172 <jsebastian.garcia1995@gmail.com>
2020-01-09 22:09:48 +0000

    printing squares

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+def __init__(self, size=0):
+    """ Size definition. """
+    self.__size = size
+
+    def area(self):
+        """ the area of the square """
+        return self.__size ** 2
+
+    def my_print(self):
+        """Custom print to the stdout a square composed of #."""
+        if self.size == 0:
+            print("")
+        else:
+            for i in range(self.size):
+                for j in range(self.size):
+                    print("#", end="")
+                print("")
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, val):
+        if not isinstance(val, int):
+            print("size must be an integer", end="")
+            raise TypeError
+        elif val < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = val
