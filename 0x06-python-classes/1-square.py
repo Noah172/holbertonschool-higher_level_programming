bcb20249fb8c2d064436f4302957b5e090b6d67b
Noah172 <jsebastian.garcia1995@gmail.com>
2020-01-09 20:48:21 +0000

    giving a value

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,6 +7,14 @@ class Square:
     """ A Square Class, has a private instance attribute: Size """
 
     def __init__(self, size=0):
-        """ Size definition. """
+        """ Size definition
+
+        Args:
+            size (int): Private instance attribute: size.
+        """
+        sif not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
         pass
