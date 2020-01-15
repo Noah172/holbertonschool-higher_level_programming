67f0c0f8291da04ba18c32e6dd8db77b2bbe2428
Noah172 <jsebastian.garcia1995@gmail.com>
2020-01-15 19:19:12 +0000

    fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,7 +5,7 @@ class Rectangle:
     """
     def __init__(self, height=0, width=0):
         """
-        using it sefl
+        Init class
         args:
         height: the height of the rectangle
         width: the width of the rectangle
@@ -15,25 +15,25 @@ class Rectangle:
         self.width = width
 
     @property
-    def height(self):
-        return self.__height
-
-    @property
     def width(self):
         return self.__width
-
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
 
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
