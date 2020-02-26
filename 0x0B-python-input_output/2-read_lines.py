e7b2709980e26ce36634de3902beef255104d74a
Noah172 <jsebastian.garcia1995@gmail.com>
2020-02-26 04:24:55 +0000

    read n lines

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -2,7 +2,13 @@
 def read_lines(filename="", nb_lines=0):
     nl = 0
     with open(filename, encoding='utf-8') as f:
-        for l in f:
-            nl += 1
-            if nl <= nb_lines:
-                print(f.read(nl))
+        txt = []
+    with open(filename, mode='r', encoding='utf-8') as filet2:
+        txt = filet2.readlines()
+    numberLines = len(txt)
+    if nb_lines <= 0 or nb_lines >= numberLines:
+        for item in txt:
+            print(item, end='')
+    else:
+        for i in range(nb_lines):
+            print(txt[i], end='')
