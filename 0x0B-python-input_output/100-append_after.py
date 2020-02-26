b0e1be3892cb7dad5c884320431a887db8f7a580
Noah172 <jsebastian.garcia1995@gmail.com>
2020-02-26 04:46:26 +0000

    append after

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+append after
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    lines = ""
+    with open(filename, 'r', encoding="utf-8") as f:
+        for line in f:
+            lines += line
+            if search_string in line:
+                lines += new_string
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(lines)
