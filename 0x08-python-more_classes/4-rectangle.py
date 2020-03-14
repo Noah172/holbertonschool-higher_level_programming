dedde92f41611dcc3bdac423a158a058533f6b16
Noah172 <jsebastian.garcia1995@gmail.com>
2020-03-14 22:24:38 +0000

    task 4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,62 +1,56 @@
 #!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
+
 class Rectangle:
-    """
-    creating class Rectangle
-    """
-    def __init__(self, height=0, width=0):
-        """
-        using it sefl
-        args:
-        height: the height of the rectangle
-        width: the width of the rectangle
-        """
-        self.__height = height
-        self.__width = width
-
-    def area(self):
-        return self.__width * self.__height
-
-    def perimeter(self):
-        P = (self.__width * 2) + (self.__height * 2)
-        return P
-
-    def __str__(self):
-        if self.__width == 0 or self.__height == 0:
-            return ("")
-
-        rectangle = []
-        for i in range(self.__width):
-            for j in range(self.__height):
-                rectangle.append("#")
-            rectangle.append("\n")
-        return ("".join(rectangle))
-
-    def __repr__(self):
-        """return a string representation of the rectangle to be able
-        to recreate a new instance by using eval() (see example below)
-        """
-        return ("Rectangle({}, {})".format(self.__width, self.__height))
-
-    @property
-    def height(self):
-        return self.__height
+    """Class"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         return self.__width
 
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
-
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return self.__width * 2 + self.__height * 2
+
+    def __str__(self):
+        if self.__width == 0 or self.height == 0:
+            return("")
+
+        rec = []
+        for i in range(self.__height):
+            [rec.append("#") for j in range(self.__width)]
+            if i is not self.__height - 1:
+                rec.append("\n")
+        return ("".join(rec))
+
+    def __repr__(self):
+        return ("Rectangle({}, {})".format(self.__width, self.__height))
