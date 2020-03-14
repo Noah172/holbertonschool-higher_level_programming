39dfb3ac5aad2c20b1ff4141c8b008004312a60e
Noah172 <jsebastian.garcia1995@gmail.com>
2020-03-14 19:28:02 +0000

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,19 +1,15 @@
-#!usr/bin/python3
-
-"""
-module for Base class
-"""
-
-
-import os
+#!/usr/bin/python3
+"""Write the first class Base:"""
 import json
+from os import path
 
 
 class Base:
-    
+    """Class base"""
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Inicialice class"""
         if id is not None:
             self.id = id
         else:
@@ -22,39 +18,44 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """Returns the JSON string"""
         if list_dictionaries is None:
             list_dictionaries = []
-        return json.dumps(list_dictionaries)
+        return(json.dumps(list_dictionaries))
 
     @classmethod
     def save_to_file(cls, list_objs):
-        file = cls.__name__+".json"
-        list = []
+        """Writes the JSON string representation"""
+        obj = []
         if list_objs:
             for i in list_objs:
-                list.append(cls.to_dictionary(i))
-        with open(file, "w") as f:
-            f.write(cls.to_json_string(list))
+                obj.append(cls.to_dictionary(i))
+        with open(cls.__name__ + ".json", "w") as file:
+            file.write(cls.to_json_string(obj))
 
     @staticmethod
     def from_json_string(json_string):
-        if json_string is None:
+        """returns the list of the JSON string"""
+        if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
         if cls.__name__ is "Rectangle":
-            obj = cls(1, 1)
+            from models.rectangle import Rectangle
+            object = Rectangle(1, 1)
         elif cls.__name__ is "Square":
-            obj = cls(1)
-        obj.update(**dictionary)
-        return obj
+            from models.square import Square
+            object = Square(1)
+        object.update(**dictionary)
+        return object
 
     @classmethod
     def load_from_file(cls):
-        file = cls.__name__ + ".json"
-        if not os.path.isfile(file):
+        """returns a list"""
+        if not path.isfile(cls.__name__ + ".json"):
             return []
-        with open(file, "r") as f:
+        with open(cls.__name__ + ".json", "r", encoding="utf-8") as f:
             return [cls.create(**d) for d in cls.from_json_string(f.read())]
