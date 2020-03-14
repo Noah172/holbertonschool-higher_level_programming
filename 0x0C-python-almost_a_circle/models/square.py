02532cb93e16a3112c175a6ef71effe6ab90d02a
Noah172 <jsebastian.garcia1995@gmail.com>
2020-03-14 19:30:24 +0000

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-
-"""
-is a Square class
-"""
-
-
+""" Square class """
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """is a square"""
+    """Write the class Square that inherits from Rectangle"""
+
     def __init__(self, size, x=0, y=0, id=None):
-        """init a square"""
-        super().__init__(size, size, x, y, id)
+        """"inicialization constructor"""
         self.size = size
+        super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
@@ -23,7 +19,7 @@ class Square(Rectangle):
     @size.setter
     def size(self, value):
         """setter function"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value <= 0:
             raise ValueError("width must be > 0")
@@ -31,39 +27,35 @@ class Square(Rectangle):
         self.__width = value
 
     def __str__(self):
-        """str function"""
-        w = self.__width
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, w)
+        """should print, and str() should return"""
+        return("[Square] ({}) {}/{} - {}".format
+               (self.id, self.x, self.y, self.width))
 
     def update(self, *args, **kwargs):
-        """str function"""
-        i = 0
-        for arg in args:
-            i += 1
-            if i == 1:
-                self.id = arg
-            if i == 2:
-                self.__width = arg
-            if i == 3:
-                self.x = arg
-            if i == 4:
-                self.y = arg
-
-        for key, value in kwargs.items():
-            if key == "id":
-                self.id = value
-            if key == "size":
-                self.__width = value
-            if key == "x":
-                self.x = value
-            if key == "y":
-                self.y = value
+        """update"""
+        for i, j in enumerate(args):
+            if i == 0:
+                self.id = j
+            elif i == 1:
+                self.size = j
+            elif i == 2:
+                self.x = j
+            elif i == 3:
+                self.y = j
+        if "id" in kwargs:
+            self.id = kwargs["id"]
+        if "size" in kwargs:
+            self.size = kwargs["size"]
+        if "x" in kwargs:
+            self.x = kwargs["x"]
+        if "y" in kwargs:
+            self.y = kwargs["y"]
 
     def to_dictionary(self):
-        """to dictionary function"""
-        dictionary = {}
-        dictionary["id"] = self.id
-        dictionary["size"] = self.width
-        dictionary["x"] = self.x
-        dictionary["y"] = self.y
-        return dictionary
+        """ dictionary test"""
+        sqr_dict = {}
+        sqr_dict["id"] = self.id
+        sqr_dict["size"] = self.size
+        sqr_dict["x"] = self.x
+        sqr_dict["y"] = self.y
+        return sqr_dict
