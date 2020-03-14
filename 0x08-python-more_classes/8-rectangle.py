037da78b84f2f515669f693681c9cb0db4e294fe
Noah172 <jsebastian.garcia1995@gmail.com>
2020-03-14 22:27:32 +0000

    task 8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -1,60 +1,70 @@
 #!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
+
 class Rectangle:
-    """
-    creating class Rectangle
-    """
+    """Class"""
     number_of_instances = 0
     print_symbol = "#"
 
-    def __init__(self, height=0, width=0):
-        """
-        using it sefl
-        args:
-        height: the height of the rectangle
-        width: the width of the rectangle
-        """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
         type(self).number_of_instances += 1
-        self.__height = height
-        self.__width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
     def area(self):
         return self.__width * self.__height
 
     def perimeter(self):
-        P = (self.__width * 2) + (self.__height * 2)
-        return P
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return self.__width * 2 + self.__height * 2
 
     def __str__(self):
-        if self.__width == 0 or self.__height == 0:
-            return ("")
+        if self.__width == 0 or self.height == 0:
+            return("")
 
-        rectangle = []
-        for i in range(self.__width):
-            for j in range(self.__height):
-                rectangle.append(str(self.print_symbol))
-            rectangle.append("\n")
-        return ("".join(rectangle))
+        rec = []
+        for i in range(self.__height):
+            [rec.append(str(self.print_symbol)) for j in range(self.__width)]
+            if i is not self.__height - 1:
+                rec.append("\n")
+        return ("".join(rec))
 
     def __repr__(self):
-        """return a string representation of the rectangle to be able
-        to recreate a new instance by using eval() (see example below)
-        """
         return ("Rectangle({}, {})".format(self.__width, self.__height))
 
     def __del__(self):
-        """Print the message Bye rectangle...
-        """
         type(self).number_of_instances -= 1
         print("Bye rectangle...")
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        """returns the biggest rectangle based on the area
-        Args:
-            rect_1 (Rectangle): rectangle 1
-            rect_2 (Rectangle): rectangle 2
-        Raises:
-        Returns:
-        """
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
@@ -62,27 +72,3 @@ class Rectangle:
         if rect_1.area() >= rect_2.area():
             return (rect_1)
         return (rect_2)
-
-    @property
-    def height(self):
-        return self.__height
-
-    @property
-    def width(self):
-        return self.__width
-
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
-
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
