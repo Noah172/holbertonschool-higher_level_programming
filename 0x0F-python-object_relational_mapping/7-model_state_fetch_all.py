af1dbd1172fd499192517c7e32e5a723c67b079b
noah172 <jsebastian.garcia1995@gmail.com>
2020-03-29 22:42:24 +0000

    task 7: first commit

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+
+"""
+if __name__ == "__main__":
+
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    if len(argv) < 4:
+        print('Usage: {} username \
+               password database_name'.format(argv[0]))
+        exit(1)
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(user, passwd, db))
+    session = sessionmaker(bind=eng)
+    States = session().query(State).all()
+
+    for row in States:
+        print("{}: {}".format(row.id, row.name))
+
+    session.close()
