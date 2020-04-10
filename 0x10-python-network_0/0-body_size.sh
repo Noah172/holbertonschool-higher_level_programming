037ac49037a15153b9103d6aed552c5b7d528001
noah172 <jsebastian.garcia1995@gmail.com>
2020-04-10 14:50:25 +0000

    size body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # script that takes in a URL and displays the size 
 # of the body of the response
-curl -s -I "$1" | grep Content-Length | cut --delimiter " " -f2
+curl -sI "$1" | grep Content-Length | cut --delimiter " " -f2
