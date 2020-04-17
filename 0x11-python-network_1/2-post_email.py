b747fff19d1d5186d0cf8f1942b5dcc41e525ed8
noah172 <jsebastian.garcia1995@gmail.com>
2020-04-17 04:55:39 +0000

    tasks 2 to 7

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    email = {"email": argv[2]}
+    with urlopen(url, urlencode(email).encode()) as response:
+        print(response.read().decode("utf-8"))
