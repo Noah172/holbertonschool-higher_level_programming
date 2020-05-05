24fb7bca0b6a71dac97f4ac31ca7791650d4645d
noah172 <jsebastian.garcia1995@gmail.com>
2020-05-05 17:17:28 -0500

    task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv;
+
+if (!args[2]) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let print = args[2]; print > 0; print--) {
+      console.log('C is fun');
+  }
+}
