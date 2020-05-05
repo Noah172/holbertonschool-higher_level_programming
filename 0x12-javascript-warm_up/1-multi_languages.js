1f576b8f2451c0afcce62e7263aceb689fa57def
noah172 <jsebastian.garcia1995@gmail.com>
2020-05-05 14:33:18 -0500

    task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 // setting variables
-let C = 'C is fun';
-let P = 'Python is cool';
-let J = 'Javascript is amazing';
+let C = 'C is fun\nPython is cool\nJavascript is amazing';
 // printing
 console.log(C);
-console.log(P);
-console.log(J);
