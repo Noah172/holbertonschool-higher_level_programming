c1df7ccc1bbfe0dc59a4896905fa646883518024
noah172 <jsebastian.garcia1995@gmail.com>
2020-05-05 17:41:36 -0500

    task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+function factorial (a) {
+  if (a === 1 || isNaN(a)) {
+    return 1;
+  }
+  return a * factorial(a - 1);
+}
+
+const args = process.argv;
+console.log(factorial(parseInt(args[2])));
