0edd40f1a5245308982d2486dfc71dc18e975334
noah172 <jsebastian.garcia1995@gmail.com>
2020-05-05 18:17:18 -0500

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-let args = process.argv;
+const args = process.argv;
 const len = process.argv.length;
 if (len <= 3) {
   console.log(0);
 } else {
-  args.sort(function(a, b){return a - b});
-  let second = args[len - 2]
+  args.sort(function (a, b) { return a - b; });
+  const second = args[len - 2];
   console.log(second);
 }
