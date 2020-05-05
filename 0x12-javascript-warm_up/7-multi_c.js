6f0f2913e4da85297376824884dfa56548dccaaf
noah172 <jsebastian.garcia1995@gmail.com>
2020-05-05 17:18:59 -0500

    identation

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,6 +5,6 @@ if (!args[2]) {
   console.log('Missing number of occurrences');
 } else {
   for (let print = args[2]; print > 0; print--) {
-      console.log('C is fun');
+    console.log('C is fun');
   }
 }
