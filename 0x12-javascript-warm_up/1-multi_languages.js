ad85eca2f3e6ff730d5633506cf57f4974574835
noah172 <jsebastian.garcia1995@gmail.com>
2020-05-05 14:31:12 -0500

    task 1: print 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// setting variables
+let C = 'C is fun';
+let P = 'Python is cool';
+let J = 'Javascript is amazing';
+// printing
+console.log(C);
+console.log(P);
+console.log(J);
