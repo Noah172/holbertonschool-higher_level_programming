9f52f4c6c5ae648461083e84c56527dea7555295
noah172 <jsebastian.garcia1995@gmail.com>
2020-05-11 17:41:00 -0500

    task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 exports.esrever = function (list) {
   const reverseList = [];
   for (let index = list.length - 1; index >= 0; index--) {
-    revList.push(list[index]);
+    reverseList.push(list[index]);
   }
   return reverseList;
 };
