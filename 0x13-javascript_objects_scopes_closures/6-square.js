e72102486fd7136e8d365a7e38496c798f2cadff
noah172 <jsebastian.garcia1995@gmail.com>
2020-05-11 17:30:24 -0500

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,7 +4,7 @@ const SquareA = require('./5-square.js');
 class Square extends SquareA {
   charPrint (c) {
     if (!c) {
-      c = "X";
+      c = 'X';
     }
     for (let height = 0; height < this.height; height++) {
       console.log(c.repeat(this.width));
