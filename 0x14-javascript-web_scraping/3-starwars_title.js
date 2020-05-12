ce40e15af329aec43303ab139496779be2e317c6
noah172 <jsebastian.garcia1995@gmail.com>
2020-05-12 16:17:49 -0500

    task 3: fixed semistandard

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,12 +3,11 @@ const request = require('request');
 const movieID = parseInt(process.argv[2]);
 const movieOfSaga = 'https://swapi-api.hbtn.io/api/films/' + movieID;
 
-request (movieOfSaga, function (error, response, body) {
+request(movieOfSaga, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    if (response.statusCode === 200)
-    {
+    if (response.statusCode === 200) {
       console.log(JSON.parse(body).title);
     }
   }
