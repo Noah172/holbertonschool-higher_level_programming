32dae592592479be7b5b76eb39b316168c0164d6
noah172 <jsebastian.garcia1995@gmail.com>
2020-05-12 16:01:41 -0500

    task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const request = require('request');
+const pag = process.argv[2];
+
+request(pag, function (error, response, body) {
+  if (error) {
+    console.log('code:', response.statusCode);
+  } else {
+    console.log('code:', response.statusCode);
+  }
+});
