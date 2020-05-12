3b856a483dcc1fbf8ac6ef473643ef3bf8814294
noah172 <jsebastian.garcia1995@gmail.com>
2020-05-12 16:47:32 -0500

    task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const fs = require('fs');
+const request = require('request')
+const url = process.argv[2];
+const fileName = process.argv[3];
+
+function writeFile (fileName, data) {
+  fs.writeFile(fileName, data, 'utf8', (err) => {
+    if (err) {
+      return console.error(err);
+    }
+  });
+}
+
+request (url, function (error, response, body) {
+  if (error) {
+    console.log('code:', response.statusCode);
+  } else {
+    writeFile(fileName, body);
+  }
+});
